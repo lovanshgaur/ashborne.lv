98005626b5993fb4e917574cfacdfaf483e37ee7
lovanshgaur <lovanshgaur@gmail.com>
2026-06-22 10:24:29 +0530

    Question 3: FizzBuzz

---
+++ q3_fizzbuzz.js
@@ -0,0 +1,21 @@
+// Question 3: FizzBuzz
+
+// Print numbers from 1 to n; for multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for numbers divisible by both 3 and 5 print "FizzBuzz".
+
+function fizzbuzz(n) {
+    const result=[]
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+     result.push("FizzBuzz")
+    } else if (i % 3 === 0) {
+        result.push("Fizz")
+    } else if (i % 5 === 0) {
+        result.push("Buzz")
+    } else {
+        result.push(i)
+    }
+  }
+  return result
+}
+
+console.log(fizzbuzz(16))
