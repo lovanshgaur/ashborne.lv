336554900f40a3dd186498b336238f65516b7e08
lovanshgaur <lovanshgaur@gmail.com>
2026-06-22 12:25:43 +0530

    Question 4: Find Duplicates

---
+++ q4_findduplicate.js
@@ -0,0 +1,21 @@
+// Question 4: Find Duplicates
+// Given an array of numbers, return all elements that appear more than once.
+
+const num = [1, 2, 2, 3, 4, 5, 4, 6, 1];
+
+function findDuplicate(num) {
+  let j = [];
+  let k = [];
+  for (let i = 0; i < num.length; i++) {
+    if (i === 0) {
+      j.push(num[0]);
+    } else if (j.includes(num[i])) {
+      k.push(num[i]);
+    } else {
+      j.push(num[i]);
+    }
+  }
+  return k
+}
+
+console.log(findDuplicate(num));
